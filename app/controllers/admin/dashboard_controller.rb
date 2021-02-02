ef14f5abe8c809fd454a6819996547a0c241a88e
Steven <schenn1992@gmail.com>
2021-02-02 19:21:08 +0000

    implement show function

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   
   def show
+    @products = Product.count
+    @categories = Category.count
+    @apparel = Category.find(id=1)
+    @electronics = Category.find(id=2)
+    @furniture = Category.find(id=3)
   end
 end
